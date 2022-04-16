ae3b06e9d62fdf979a5030af0182499b631ad229
sreedhar <sreedharks2003@gmail.com>
2022-04-16 20:13:36 +0530

    testing to fetch images from twitter api

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import React,{ useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ActivityIndicator,ScrollView,Image,FlatList } from 'react-native';
 
+import MediaComponent from './Components/MediaComponent';
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <MediaComponent/>
+    </View>  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center'
   },
 });
---
+++ Components/MediaComponent.js
@@ -0,0 +1,59 @@
+import React,{ useEffect } from 'react';
+import {StyleSheet, Text, View,ActivityIndicator,ScrollView,Image,FlatList} from 'react-native';
+import env from '../env'
+export default function MediaComponent(){
+    const bearerToken = env.btoken.toString();
+    const [Data,setData]=React.useState([]);
+    const [loading,setLoading]=React.useState(true);
+    const apiRequest=()=>{
+    fetch('https://api.twitter.com/2/tweets/search/all?query=images&expansions=attachments.media_keys&media.fields=url',{
+        method:'GET',
+        headers:{
+        
+        'Authorization':`Bearer ${bearerToken}`,
+        'Content-Type':'application/json'
+        }
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        setData(data.includes.media);
+        setLoading(false);
+    })
+    .catch(e=>{console.log(e)})
+    }
+    useEffect(()=>{
+    apiRequest();
+    },[])   
+    return(
+        <View style={styles.container}>
+        {loading==true ? <ActivityIndicator color={'black'} /> :(
+            <FlatList 
+             data={Data}
+             keyExtractor={(item)=>{item.media_key.toString()}}
+             renderItem={(item)=>{
+                 console.log(item.item);
+               return(
+                   <View style={{flexDirection:'column'}} > 
+                    <Text>{item.item.type}</Text>
+                     
+                    {item.item.type!="video" &&  <Image source={{uri:item.item.url}} style={{height:550,width:550}}/>}
+                  </View>
+               );
+             }}
+            />
+          ) 
+          }
+          </View>
+    ); 
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginTop:50,
+        flexDirection:'column',
+        margin:5,
+       
+
+    }
+})
+
